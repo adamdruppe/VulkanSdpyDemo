2f85676d49282146d45891683ce411702c071e52
Adam D. Ruppe <destructionator@gmail.com>
2023-09-14 14:46:50 -0400

    works on linux w/ ldc

---
+++ source/vulkan_xlib.d
@@ -0,0 +1,9 @@
+module vulkan_xlib;
+
+import arsd.simpledisplay;
+import erupted.platform_extensions;
+
+mixin Platform_Extensions!USE_PLATFORM_XLIB_KHR;
+
+                //PFN_vkCreateXlibSurfaceKHR                                            vkCreateXlibSurfaceKHR;
+                //PFN_vkGetPhysicalDeviceXlibPresentationSupportKHR                     vkGetPhysicalDeviceXlibPresentationSupportKHR;
